24cc01438417b4b2b36b486bdea9ead8bd5a5136
scottoconnor <38475383+scottoconnor@users.noreply.github.com>
2018-07-13 12:54:15 -0400

    Add files via upload

---
+++ courses.pl
@@ -0,0 +1,83 @@
+#
+# Par and slope for each course.
+#
+# Copyright (c) 2018 Scott O'Connor
+#
+%c = (
+	# South Front
+	SF => {
+		#
+		# Pars for each hole on each course.
+		#
+                1 => 4,
+		2 => 4,
+		3 => 3,
+		4 => 4,
+		5 => 5,
+		6 => 5,
+                7 => 3,
+		8 => 4,
+		9 => 4,
+		#
+		# USGA
+		par => 34.8, slope => 127,
+	},
+	# South Back
+	SB => {
+		#
+		# Pars for each hole on each course.
+		#
+                1 => 5,
+		2 => 3,
+		3 => 4,
+		4 => 4,
+		5 => 5,
+		6 => 3,
+                7 => 4,
+		8 => 3,
+		9 => 5,
+		#
+		# USGA
+		par => 34.7, slope => 121,
+	},
+	# North Front
+	NF => {
+		#
+		# Pars for each hole on each course.
+		#
+                1 => 5,
+		2 => 4,
+		3 => 4,
+		4 => 4,
+		5 => 5,
+		6 => 3,
+                7 => 4,
+		8 => 3,
+		9 => 4,
+		#
+		# USGA
+		par => 35.6, slope => 125,
+	},
+	# North Back
+	NB => {
+		#
+		# Pars for each hole on each course.
+		#
+                1 => 4,
+		2 => 4,
+		3 => 5,
+		4 => 3,
+		5 => 4,
+		6 => 4,
+                7 => 3,
+		8 => 4,
+		9 => 5,
+		#
+		# USGA
+		par => 35.1, slope => 130,
+	},
+	AVE => {
+		# USGA average of above four courses.
+		par => 35.1, slope => 125,
+	}
+);
