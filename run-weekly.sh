1f20ca1a3e2a2a9fdc7f0cdafeed43426b1b6c80
Scott O'Connor <scott.oconnor@oracle.com>
2018-08-16 12:14:29 -0400

    add command line arg for week number

--- run-weekly.sh
+++ run-weekly.sh
@@ -4,26 +4,21 @@
 # Copyright (c) 2018 Scott O'Connor
 #
 
-if [ -f /tmp/me ]; then
-	rm /tmp/me
+if [ -z $1 ]; then
+	echo "usuage: run-weekly.sh <week number>"
+	exit
 fi
 
-WEEK=11
+WEEK=${1}
 START_YEAR=2018
 END_YEAR=2010
 
 YEAR=${START_YEAR}
 
-if [ -f /tmp/${YEAR}.html ]; then
-	rm /tmp/${YEAR}.html
-fi
+rm -f /tmp/${YEAR}*.html
 
 ./skperf.pl -y -s -i -h -sy ${YEAR} -ey ${YEAR} > /tmp/${YEAR}.html
 
-
-if [ -f /tmp/${START_YEAR}-${END_YEAR}-week${WEEK}.html ] ; then
-	rm /tmp/${START_YEAR}-${END_YEAR}-week${WEEK}.html
-fi
 
 until [  $YEAR -lt $END_YEAR ]; do
 	./skperf.pl -y -s -i -h -sy $YEAR -ey $YEAR -sw 1 -ew $WEEK >> /tmp/${START_YEAR}-${END_YEAR}-week${WEEK}.html
@@ -32,10 +27,6 @@ done
 
 
 YEAR=${START_YEAR}
-
-if [ -f /tmp/${YEAR}-weekly.html ]; then
-	rm /tmp/${YEAR}-weekly.html
-fi
 
 until [  ${WEEK} -lt 1 ]; do
 	./skperf.pl -h -s -i -w -y -sy $YEAR -ey $YEAR -sw ${WEEK} -ew ${WEEK} >> /tmp/${YEAR}-weekly.html
