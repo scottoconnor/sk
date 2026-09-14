0d97c31d4b9b63174047bc23d1d7006da00dfa46
Scott O'Connor <sto@fastmail.net>
2026-09-14 15:53:16 -0400

    A lot of clean up, but a lot more work to do. Need to enable 'strict' and 'warnings'

--- overall-stats.pl
+++ overall-stats.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 #
-# Copyright (c) 2018, 2022 Scott O'Connor
+# Copyright (c) 2018, 2026 Scott O'Connor
 #
 
 use Getopt::Long;
@@ -22,43 +22,43 @@ if ($out) {
     select STDOUT;
 }
 
-@line = `./skperf.pl -t -sy 2003 -ey $year | grep -A 1 "Birdie Table"`;
-$num = @line;
-$x = 0;
+#@line = qx{./skperf.pl -t -sy 2003 -ey $year | grep -A 1 "Birdie Table"};
+#$num = @line;
+#$x = 0;
 
-print "Yearly Birdie winners\n";
-print "---------------------\n";
-while ($x < $num) {
-    chop ($line[$x]);
-    print "$line[$x] ";
-    $x++;
-    chop ($line[$x]);
-    print "$line[$x]\n";
-    $x++; $x++;
-}
+#print "Yearly Birdie winners\n";
+#print "---------------------\n";
+#while ($x < $num) {
+    #chop ($line[$x]);
+    #print "$line[$x] ";
+    #$x++;
+    #chop ($line[$x]);
+    #print "$line[$x]\n";
+    #$x++; $x++;
+#}
 
-print "\n\n";
+#print "\n\n";
 
-@line = `./skperf.pl -t -sy 2003 -ey $year | grep -A 1 "Eagle Table"`;
-$num = @line;
-$x = 0;
+#@line = qx{./skperf.pl -t -sy 2003 -ey $year | grep -A 1 "Eagle Table"};
+#$num = @line;
+#$x = 0;
 
-print "Yearly Eagle winners\n";
-print "--------------------\n";
-while ($x < $num) {
-    chop ($line[$x]);
-    print "$line[$x] ";
-    $x++;
-    chop ($line[$x]);
-    print "$line[$x]\n";
-    $x++; $x++;
-}
+#print "Yearly Eagle winners\n";
+#print "--------------------\n";
+#while ($x < $num) {
+    #chop ($line[$x]);
+    #print "$line[$x] ";
+    #$x++;
+    #chop ($line[$x]);
+    #print "$line[$x]\n";
+    #$x++; $x++;
+#}
 
 print "\n\n";
 
 print "$year Most Improved (best to not so best)\n";
 print "----------------------------------------\n";
-@line = `./skperf.pl -m -y $year`;
+@line = qx{./skperf.pl -m -y $year};
 print @line;
 
 print "\n\n";
@@ -69,7 +69,7 @@ print "\n\n";
 #
 print "$year player net average scoring vs. par\n";
 print "---------------------------------------\n";
-@line = `./skperf.pl -vhc -y $year`;
+@line = qx{./skperf.pl -vhc -y $year};
 @nline = grep(/Ave = /, @line);
 print @nline;
 
@@ -77,7 +77,7 @@ print "\n\n";
 
 print "$year Player week by week stats\n";
 print "------------------------------\n";
-@line = `./skperf.pl -vhc -y $year`;
+@line = qx{./skperf.pl -vhc -y $year};
 @nline = grep(/net /, @line);
 print @nline;
 
@@ -85,21 +85,21 @@ print "\n\n";
 
 print "$year Course Stats\n";
 print "-----------------\n";
-@line = `./skperf.pl -c -y $year`;
+@line = qx{./skperf.pl -c -y $year};
 print @line;
 
 print "\n\n";
 
 print "$year Hardest to Easiest Holes\n";
 print "-----------------------------\n";
-@line = `./skperf.pl -H -y $year`;
+@line = qx{./skperf.pl -H -y $year};
 print @line;
 
 print "\n\n";
 
 print "$year Hardest to Easiest Holes Per Nine\n";
 print "--------------------------------------\n";
-@line = `./skperf.pl -H -y $year`;
+@line = qx{./skperf.pl -H -y $year};
 @nline = grep(/South Front/, @line);
 print @nline, "\n\n";
 @nline = grep(/South Back/, @line);
@@ -107,7 +107,7 @@ print @nline, "\n\n";
 @nline = grep(/North Front/, @line);
 print @nline, "\n\n";
 @nline = grep(/North Back/, @line);
-print @nline, "\n\n";
+print @nline;
 
 
 $low_net = 25;
@@ -115,7 +115,7 @@ $high_net = 60;
 
 print "$year Lowest to Highest net scores\n";
 print "---------------------------------\n";
-@line = `./skperf.pl -vhc -y $year`;
+@line = qx{./skperf.pl -vhc -y $year};
 for ($low_net = 25; $low_net <= $high_net; $low_net++) {
     $num = grep(/net $low_net/, @line);
     if ($num > 0) {
@@ -130,9 +130,9 @@ print "\n\n";
 print "Top 5 by year: Lowest to Highest average net scores\n";
 print "(need at least 10 rounds to qualify)\n";
 print "---------------------------------------------------\n";
-for ($y = 2003; $y <= $year; $y++) {
+for (my $y = 2003; $y <= $year; $y++) {
     print "$y\n";
-    @line = `./skperf.pl -vhc -y $y grep "net = "`;
+    @line = qx{./skperf.pl -vhc -y $y grep "net = "};
     $cnt = 0;
     while (($temp_line = shift @line) && ($cnt < 5)) {
         chomp($temp_line);
@@ -149,42 +149,43 @@ print "\n\n";
 
 print "$year 30's Club\n";
 print "--------------\n";
-@line = `./skperf.pl -g -y $year`;
+@line = qx{./skperf.pl -g -y $year};
 print @line;
 
 print "\n\n";
 
 print "$year Others break down\n";
 print "----------------------\n";
-@line = `./skperf.pl -o -y $year`;
+@line = qx{./skperf.pl -o -y $year};
 print @line;
 
 print "\n\n";
 
 print "$year detailed player stats\n";
 print "--------------------------\n";
-@line = `./skperf.pl -p -y $year`;
+@line = qx{./skperf.pl -p -y $year};
 print @line;
 
 print "\n\n";
 
-print "2003-$year detailed player stats\n";
+
+print "2003-2024 detailed player stats\n";
 print "-------------------------------\n";
-@line = `./skperf.pl -p -sy 2003`;
+@line = qx{./skperf.pl -p -sy 2003 -ey 2024};
 print @line;
 
 print "\n\n";
 
-print "2003-$year detailed player stats\n";
+print "2025 detailed player stats\n";
 print "-------------------------------\n";
-@line = `./skperf.pl -p -sy 2003 -ey $year`;
+@line = qx{./skperf.pl -p -y 2025};
 print @line;
 
 print "\n\n";
 
 print "All time stats (1997-$year)\n";
 print "--------------------------\n";
-@line = `./skperf.pl -at -ey $year`;
+@line = qx{./skperf.pl -at -ey $year};
 print @line;
 
 if ($out) {
